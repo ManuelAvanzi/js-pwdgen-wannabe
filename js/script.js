141783c08fdd1928cdea2a29192c66aee0373098
ManuelAvanzi <manuel.92.avanzi@gmail.com>
2021-12-02 14:30:11 +0100

    istruzioni

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// ---------------PASSWORD GENERATOR--------
+
+//  1--- creo 4 variabili vuote :
+//              nome
+//              cognome
+//              colore
+//              password
+
+//  2---- creo una variabile conentente il numero 21
+//             const num=21;
+
+//  3--- per ogni varibile chiedo al utente di inserire un dato tramite il la funzione prompt();
+      // per ogni dato inserito aggiorno la variabile password 
+         //   password=password+ contentuto delle variabile nome;
+         //   password=password+ contentuto delle variabile cognome;
+         //   password=password+ contentuto delle variabile colore;
+
+// 4--- aggiungo a password il contenuto di num
+        // password+=num;
+
+    
+// stampo il valore tramite innerHTML o textContent;
