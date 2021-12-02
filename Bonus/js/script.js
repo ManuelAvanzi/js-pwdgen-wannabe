9a9d4a6aafd9777fec9547412231ac623af2575a
ManuelAvanzi <manuel.92.avanzi@gmail.com>
2021-12-02 21:04:53 +0100

    sostituzione caratteri non validi con caratteri validi estratti a sorte

--- Bonus/js/script.js
+++ Bonus/js/script.js
@@ -2,19 +2,27 @@
 // cosi da creare una password che sia memorizzabile come un colore 
 // stampo a video il colore e la password 
 
-const unvalidChar=['g','h','i','l','m','n','o','p','q','r','s','t','u','v','x','y','z'];
-
-const button=document.getElementById("restart").addEventListener("click",function(){
-
+const validCharSet=['a','b','c','d','e','f','0','1','2','3','4','5','6','7','8','9'];
+const illegalCharSet=['g','h','i','l','m','n','o','p','q','r','s','t','u','v','x','y','z'];
 const box=document.getElementById("box");
 
 
+
+const button=document.getElementById("restart").addEventListener("click",function(){
+
+
+ 
 let firstName=prompt("Inserisci il tuo nome");
 let lastName=prompt("Inseirsci il tuo cognome");
-let Year=prompt("Inserisci l'anno di nascita");
+let year=prompt("Inserisci l'anno di nascita");
 
-let grezza=firstName+lastName+Year;   // password grezza 
 
+  // controllo che l'età sia un numero plausibile
+while(parseInt(year)>2020 || parseInt(year)<1800){
+    year=prompt("sei sicuro ?? inserisci una data valida");
+}
+
+let grezza=firstName+lastName+year;   // password grezza 
 let passwordEx=[];
 
 
@@ -26,12 +34,17 @@ for(let i=0;i<grezza.length;i++){
 
 
 
+                  
  // ELABORO LA PASSWORD GREZZA PER SOSTITUIRE I CARATTERI NON VALIDI
+
 for(let i=0;i<passwordEx.length;i++){
-    for(let x=0;x<unvalidChar.length;x++){
-       if(passwordEx[i]==unvalidChar[x]){
-           // se è tra quelli non validi brutalmente lo sostituisco con A
-           passwordEx[i]="A";
+    for(let x=0;x<illegalCharSet.length;x++){
+
+         //se un valore non è un carattere valido in esadecimale lo sostituisco con un valido 
+         // estratto casualmente dalla lista dei caratteri validi 
+         
+       if(passwordEx[i]==illegalCharSet[x]){
+           passwordEx[i]=validCharSet[Math.floor(Math.random()*validCharSet.length)];;
        }
     }
 }
